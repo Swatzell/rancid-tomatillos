dae47aec0b40da426e6152bd39261d3c9c037390
David Swatzell <thatsstrikethree@gmail.com>
2024-05-09 17:13:31 -0500

    add function for moviecards

--- src/Component/MovieCard/MovieCards.js
+++ src/Component/MovieCard/MovieCards.js
@@ -1,1 +1,10 @@
 import './MovieCards.css'
+
+function movieCards({ imagePath }){
+    return (
+      <div className='movie-card'>
+       <img src={imagePath} alt="Movie Poster"/>
+      </div>
+    )
+  }
+  export default movieCards;
