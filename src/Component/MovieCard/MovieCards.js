7e6a13e4bf969e1caa457bc7e96d3a0e44051bc1
David Swatzell <thatsstrikethree@gmail.com>
2024-05-10 09:51:53 -0500

    destructure and add rating and title to moviecards

--- src/Component/MovieCard/MovieCards.js
+++ src/Component/MovieCard/MovieCards.js
@@ -1,10 +1,13 @@
 import './MovieCards.css'
 
 
-function MovieCards({ movie }){
+function MovieCards(props){
+    const {movie} = props
     return (
       <div className="movie-cards">
         <img src={movie.poster_path} alt={movie.title} />
+        <h3>{movie.title}</h3>
+        <p>⭐️ {movie.average_rating}</p>
       </div>
     );
   };
