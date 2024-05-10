48fe38132e2b03110a0c319e16712101d1784401
Lydia S <149545941+LISims88@users.noreply.github.com>
2024-05-09 19:57:09 -0500

    Add the main movie poster.

--- src/Component/App/App.js
+++ src/Component/App/App.js
@@ -2,23 +2,26 @@ import './App.css';
 import { useState } from 'react';
 import MovieCards from '../MovieCard/MovieCards.js';
 import movieData from '../MockData/mockData.js';
-
+import MainMovie from '../MainMovie/MainMovie.js'
 
 function App() {
    
-   const [movies,] = useState(movieData.movies);
+  const [movies] = useState(movieData.movies);
+  const randomIndex = Math.floor(Math.random() * movies.length);
+  const randomMovie = movies[randomIndex]
     
-      return (
-        <main className="App">
-          <h1>Rancid Tomatillos</h1>
-          <div className="movie-container">
-            {movies.map((movie) => (
-              <MovieCards key={movie.id} movie={movie} />
-            ))}
-          </div>
-        </main>
-      );
-    };
+  return (
+    <main className="App">
+      <h1>Rancid Tomatillos</h1>
+      <MainMovie movie = {randomMovie} />
+      <div className="movie-container">
+        {movies.map((movie) => (
+          <MovieCards key={movie.id} movie={movie} />
+        ))}
+      </div>
+    </main>
+  );
+};
     
 
 
