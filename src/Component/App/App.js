30273b671031191accfc2d9127ee187b74a22693
David Swatzell <thatsstrikethree@gmail.com>
2024-05-11 12:25:24 -0500

    pass onClick to the moviecards function so that it takes you to the movie info page when clicked

--- src/Component/App/App.js
+++ src/Component/App/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import { useState } from 'react';
 import MovieCards from '../MovieCard/MovieCards.js';
 import movieData from '../MockData/mockData.js';
@@ -7,47 +6,50 @@ import SelectedMovie from '../SelectedMovie/SelectedMovie.js';
 
 function App() {
   const [movies] = useState(movieData.movies);
-  const [selectedMovie, setSelectedMovie] = useState(null)
-  const [showMainMovie, setShowMainMovie] = useState(true)
-  const [showMovieCards, setMovieCards]= useState(true)
+  const [selectedMovie, setSelectedMovie] = useState(null);
+  const [showMainMovie, setShowMainMovie] = useState(true);
+  const [showMovieCards, setShowMovieCards] = useState(true);
 
   const randomIndex = Math.floor(Math.random() * movies.length);
-  const randomMovie = movies[randomIndex]
+  const randomMovie = movies[randomIndex];
     
-  function handleMovieSelection (id){
-    const movie = movies.find(movie => movie.id ===id)
+  function handleMovieSelection(id) {
+    console.log("handleMovieSelection called with ID:", id);
+    const movie = movies.find(movie => movie.id === id);
+    console.log("Selected movie:", movie);
     setSelectedMovie(movie);
-    setShowMainMovie(false)
-    setMovieCards(false)
+    setShowMainMovie(false);
+    setShowMovieCards(false);
   }
+
   function handleBackToSelection() {
     setSelectedMovie(null);
     setShowMainMovie(true);
-    setMovieCards(true);
+    setShowMovieCards(true);
   }
 
-    return (
-      <main className="App">
-        <h1>Rancid Tomatillos</h1>
-        {showMainMovie && (
-          <MainMovie key ={randomMovie.id} movie={randomMovie} onClick={() => handleMovieSelection(randomMovie.id)} />
-        )}
-        {selectedMovie && <SelectedMovie movie={selectedMovie} onBack={handleBackToSelection} />}
-        {showMovieCards && (
-          <div className="movie-container">
-            {movies.map((movie) => (
-              <MovieCards
-                key={movie.id}
-                movie={movie}
-                onClick={() => handleMovieSelection(movie.id)}
-              />
-            ))}
-          </div>
-        )}
-      </main>
-    );
-};
-    
+  console.log("Selected movie state:", selectedMovie);
 
+  return (
+    <main className="App">
+      <h1>Rancid Tomatillos</h1>
+      {showMainMovie && (
+        <MainMovie key={randomMovie.id} movie={randomMovie} onClick={() => handleMovieSelection(randomMovie.id)} />
+      )}
+      {selectedMovie && <SelectedMovie movie={selectedMovie} onBack={handleBackToSelection} />}
+      {showMovieCards && (
+        <div className="movie-container">
+          {movies.map((movie) => (
+            <MovieCards
+              key={movie.id}
+              movie={movie}
+              onClick={() => handleMovieSelection(movie.id)}
+            />
+          ))}
+        </div>
+      )}
+    </main>
+  );
+};
 
 export default App;
