60c9617d33d580a792aea18f054532dc954fe856
David Swatzell <thatsstrikethree@gmail.com>
2024-05-11 13:59:38 -0500

    add functions for fetching movies and handling the movie selection with API calls as well as adding div for error display if needed

--- src/Component/App/App.js
+++ src/Component/App/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect,useState } from 'react';
 import MovieCards from '../MovieCard/MovieCards.js';
 import MainMovie from '../MainMovie/MainMovie.js'
 import SelectedMovie from '../SelectedMovie/SelectedMovie.js';
@@ -8,23 +8,39 @@ function App() {
   const [selectedMovie, setSelectedMovie] = useState(null);
   const [showMainMovie, setShowMainMovie] = useState(true);
   const [showMovieCards, setShowMovieCards] = useState(true);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     fetchMovies();
   }, []);
 
-  const randomIndex = Math.floor(Math.random() * movies.length);
-  const randomMovie = movies[randomIndex];
+  const fetchMovies = async () => {
+    try {
+      const response = await fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies');
+      if (!response.ok) {
+        throw new Error('Failed to fetch movies');
+      }
+      const data = await response.json();
+      setMovies(data.movies);
+    } catch (error) {
+      setError(error);
+    }
+  };
     
-  function handleMovieSelection(id) {
-    console.log("handleMovieSelection called with ID:", id);
-    const movie = movies.find(movie => movie.id === id);
-    console.log("Selected movie:", movie);
-    setSelectedMovie(movie);
-    setShowMainMovie(false);
-    setShowMovieCards(false);
-  }
-
+const handleMovieSelection = async (id) => {
+    try {
+      const response = await fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`);
+      if (!response.ok) {
+        throw new Error('Failed to fetch movie details');
+      }
+      const data = await response.json();
+      setSelectedMovie(data.movie);
+      setShowMainMovie(false);
+      setShowMovieCards(false);
+    } catch (error) {
+      setError(error);
+    }
+  };
   function handleBackToSelection() {
     setSelectedMovie(null);
     setShowMainMovie(true);
@@ -36,9 +52,8 @@ function App() {
   return (
     <main className="App">
       <h1>Rancid Tomatillos</h1>
-      {showMainMovie && (
-        <MainMovie key={randomMovie.id} movie={randomMovie} onClick={() => handleMovieSelection(randomMovie.id)} />
-      )}
+      {error && <div>Error: Failed to fetch data. Please try again later.</div>}
+      {showMainMovie && movies.length > 0 && <MainMovie movie={movies[0]} onClick={() => handleMovieSelection(movies[0].id)} />}
       {selectedMovie && <SelectedMovie movie={selectedMovie} onBack={handleBackToSelection} />}
       {showMovieCards && (
         <div className="movie-container">
@@ -53,7 +68,7 @@ function App() {
       )}
     </main>
   );
-};
+}
 
 export default App;
 
