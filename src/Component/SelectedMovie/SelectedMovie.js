d01e07f45f0ae2c2aab143f2fbd9839e247d0d95
Lydia S <149545941+LISims88@users.noreply.github.com>
2024-05-12 11:59:34 -0500

    Change background color and font color.

--- src/Component/SelectedMovie/SelectedMovie.js
+++ src/Component/SelectedMovie/SelectedMovie.js
@@ -1,30 +1,30 @@
 import  './SelectedMovie.css'
 
-function SelectedMovie (props) {
-const {movie, onBack} = props
+function SelectedMovie(props) {
+    const { movie, onBack } = props;
     return (
-        <div className='selectedMovie'>
-            <div className='backdrop'>
-                <img src= {movie.backdrop_path} alt = {movie.title}/>
-            </div>
-            <div className='poster'>
-                <img src= {movie.poster_path} alt = {movie.title}/>
-             </div>
-             <div className='movieSpecs'>
-                <h3>{movie.title}</h3>
-                <h4>{movie.average_rating.toFixed(2)}</h4>
-                <h4>{movie.release_date}</h4>
-                <div>
-                    <p>{movie.genres}</p>
-                    <p>{movie.overview}</p>
-                    <p>{movie.runtime}</p>
-                </div>
-             </div>
-             <span style={{ cursor: 'pointer' }} onClick={onBack}>Go Back!</span>
+      <div className='selectedMovie'>
+        <div className='imagesContainer'>
+          <div className='poster'>
+            <img src={movie.poster_path} alt={movie.title} />
+          </div>
+          <div className='backdrop'>
+            <img src={movie.backdrop_path} alt={movie.title} />
+          </div>
         </div>
-    )
-
-
+        <div className='movieSpecs'>
+          <h3>{movie.title}</h3>
+          <h4>{movie.average_rating.toFixed(2)}</h4>
+          <h4>{movie.release_date}</h4>
+          <div>
+            <p>{movie.genres}</p>
+            <p>{movie.overview}</p>
+            <p>{movie.runtime}</p>
+          </div>
+        </div>
+        <span style={{ cursor: 'pointer' }} onClick={onBack}>Go Back!</span>
+      </div>
+    );
 }
 
 export default SelectedMovie
