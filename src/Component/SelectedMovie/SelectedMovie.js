532ad7fc8b052fb7cc701a3781c63d6306993ba3
Lydia S <149545941+LISims88@users.noreply.github.com>
2024-05-14 16:07:42 -0500

    Fix the SelectedMovie's styling.

--- src/Component/SelectedMovie/SelectedMovie.js
+++ src/Component/SelectedMovie/SelectedMovie.js
@@ -1,31 +1,35 @@
 import  './SelectedMovie.css'
 
 function SelectedMovie(props) {
-    const { movie, onBack } = props;
-    const genres = movie.genres.join(', ')
-    return (
-      <div className='selectedMovie'>
-        <div className='imagesContainer'>
+  const { movie, onBack } = props;
+  const genres = movie.genres.join(', ');
+  
+  const backdropStyle = {
+    backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8)), url(${movie.backdrop_path})`,
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      backgroundRepeat: 'no-repeat'
+  };
+
+  return (
+      <div className='selectedMovie' style={backdropStyle}>
           <div className='poster'>
-            <img src={movie.poster_path} alt={movie.title} />
+              <img src={movie.poster_path} alt={movie.title} />
           </div>
-          <div className='backdrop'>
-            <img src={movie.backdrop_path} alt={movie.title} />
+          <div className='movieSpecs'>
+              <h3>{movie.title}</h3>
+              <h4>⭐️ {movie.average_rating.toFixed(2)}</h4>
+              <h4>Released: {movie.release_date}</h4>
+              <div>
+                  <p>Genres: {genres}</p>
+                  <p>Overview: {movie.overview}</p>
+                  <p>Movie Length: {movie.runtime} min.</p>
+              </div>
           </div>
-        </div>
-        <div className='movieSpecs'>
-          <h3>{movie.title}</h3>
-          <h4>⭐️ {movie.average_rating.toFixed(2)}</h4>
-          <h4>Released: {movie.release_date}</h4>
-          <div>
-            <p>Genres: {genres}</p>
-            <p>Overview: {movie.overview}</p>
-            <p>Movie Length: {movie.runtime} min.</p>
-          </div>
-        </div>
-        <span style={{ cursor: 'pointer' }} onClick={onBack}>Go Back!</span>
+          <span style={{ cursor: 'pointer' }} onClick={onBack}>Go Back!</span>
       </div>
-    );
+  );
 }
+
 
 export default SelectedMovie
