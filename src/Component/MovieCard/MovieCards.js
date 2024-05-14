817d428a9d0a490c8495c5dbd1f6bee43dd7d1c5
Lydia S <149545941+LISims88@users.noreply.github.com>
2024-05-14 17:35:52 -0500

    Fix the bug of cards being unable to click.

--- src/Component/MovieCard/MovieCards.js
+++ src/Component/MovieCard/MovieCards.js
@@ -3,9 +3,13 @@ import './MovieCards.css'
 
 function MovieCards(props){
     const {movie, onClick} = props
+    const handleClick = (event) =>{
+      event.stopPropagation();
+      onClick()
+    }
     return (
-      <div className="movie-cards" onClick={onClick}>
-        <img src={movie.poster_path} alt={movie.title} />
+      <div className="movie-cards" onClick={handleClick}>
+        <img src={movie.poster_path} alt={`${movie.title} poster`} />
         <h3>{movie.title}</h3>
         <p>⭐️ {movie.average_rating.toFixed(2)}</p>
       </div>
