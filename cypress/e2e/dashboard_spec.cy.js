683b0e27eba65af4867960609dec80ddbcd1588e
David Swatzell <thatsstrikethree@gmail.com>
2024-05-15 16:02:16 -0500

    add mock data to the example json fixture and setup first cypress test

--- cypress/e2e/dashboard_spec.cy.js
+++ cypress/e2e/dashboard_spec.cy.js
@@ -1,14 +1,15 @@
 describe('Main Page', () => {
   beforeEach(() => {
-    cy.intercept("GET", "https://rancid-tomatillos.herokuapp.com/api/v2/movies", {
-      statusCode: 200,
-      
+    cy.fixture('example').then((data) => {
+      cy.intercept("GET", "https://rancid-tomatillos.herokuapp.com/api/v2/movies", {  
+        statusCode: 200,
+        body: data,
+      })
     })
-    .visit("http://localhost:3000/")
+    cy.visit("http://localhost:3000/")
   });
 
-  it("should have a main title,main movie card and displayed movie cards with title and rating that are clickable", () => {
+  it("should have a main title, main movie card and displayed movie cards with title and rating that are clickable", () => {
     cy.contains("h1", "Rancid Tomatillos")
-   
   });
 })
