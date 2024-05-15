5d94580caff639bb2b4409780ea8ec40a0605e9c
David Swatzell <thatsstrikethree@gmail.com>
2024-05-15 17:54:23 -0500

    add testing for displaying movie collection on main page

--- cypress/e2e/dashboard_spec.cy.js
+++ cypress/e2e/dashboard_spec.cy.js
@@ -1,15 +1,57 @@
+// describe('Main Page', () => {
+//   beforeEach(() => {
+//     cy.fixture('example').then((data) => {
+//       cy.intercept("GET", "https://rancid-tomatillos.herokuapp.com/api/v2/movies",{  
+//         statusCode: 200,
+//         body: data,
+//       })
+//     })
+//     cy.visit("http://localhost:3000/")
+//   });
+
+//   it("should have a main title, main movie card and displayed movie cards with title and rating that are clickable", () => {
+//     cy.contains("h1", "Rancid Tomatillos")
+//   });
+// })
+
+// describe('Movie Info Page', () => {
+//   beforeEach(() => {
+//     cy.fixture('example').then((data) => {
+//       cy.intercept("GET",`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`, {  
+//         statusCode: 200,
+//         body: data,
+//       })
+//     })
+//     cy.visit("http://localhost:3000/")
+//   });
+// })
+
 describe('Main Page', () => {
   beforeEach(() => {
     cy.fixture('example').then((data) => {
-      cy.intercept("GET", "https://rancid-tomatillos.herokuapp.com/api/v2/movies", {  
+      cy.intercept("GET", "https://rancid-tomatillos.herokuapp.com/api/v2/movies", {
         statusCode: 200,
         body: data,
-      })
-    })
-    cy.visit("http://localhost:3000/")
+      });
+
+      data.movies.forEach((movie) => {
+        cy.intercept("GET", `https://rancid-tomatillos.herokuapp.com/api/v2/movies/${movie.id}`, {
+          statusCode: 200,
+          body: { movie },
+        });
+      });
+    });
+
+    cy.visit("http://localhost:3000/");
   });
 
-  it("should have a main title, main movie card and displayed movie cards with title and rating that are clickable", () => {
-    cy.contains("h1", "Rancid Tomatillos")
+  it("should have a main title", () => {
+    cy.contains("h1", "Rancid Tomatillos");
   });
+
+  it("should display a collection of movies", () => {
+    cy.get('.movie-container').should('be.visible');
+    cy.get('.movie-container').find('.movie-cards').should('have.length.greaterThan', 0);
+  });
+  
 })
