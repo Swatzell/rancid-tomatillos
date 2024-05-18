1203c8d17220843833fce5958a51ee73871e0a8b
David Swatzell <thatsstrikethree@gmail.com>
2024-05-18 13:48:02 -0500

    refactor cypress and add additional needed testing

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,12 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      
     },
   },
 });
+module.exports = {
+  e2e: {
+    baseUrl: 'http://localhost:3000',
+  },
+};
